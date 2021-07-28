9f2ee3054df8eab3f3b4ccb290f61b29718aa774
Anton <golubin1990@gmail.com>
2021-07-28 15:21:08 -0400

    Changed constant values to be imported from constants.js module

--- client.js
+++ client.js
@@ -1,9 +1,11 @@
 const net = require('net');
+const {IP, PORT} = require('./constants')
+console.log(IP, PORT)
 
 const connect = function () {
   const conn = net.createConnection({
-    host: '135.23.223.133',
-    port: 50542
+    host: IP,
+    port: PORT
   });
 // interpret incoming data as text
   conn.setEncoding("utf8");
