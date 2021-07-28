99b8675be97947491c8be2fae8ff2d360bdffdca
Anton <golubin1990@gmail.com>
2021-07-28 15:12:30 -0400

    Final update to add an ability to give a snake commands throught a comand line

--- input.js
+++ input.js
@@ -1,19 +1,30 @@
-const setupInput = function () {
+let connection;
+
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
-  stdin.on("data", handleUserInput);
+  stdin.on("data", (key) => {
+    if (key === '\u0003') {
+      process.exit();
+    }
+      if (key === 'w'){
+        connection.write("Move: up");
+      } else if (key === 'a'){
+        connection.write("Move: left")
+      } else if (key === 's'){
+        connection.write("Move: down");
+      } else if (key === 'd'){
+        connection.write('Move: right');
+      }
+      process.stdout.write('\r');
+      process.stdout.write(key);
+      }
+  );
   return stdin;
 };
 
-const handleUserInput = function (key) {
-  if (key === '\u0003') {
-    process.exit();
-  }
-  process.stdout.write('\r');
-  process.stdout.write(key);
-};
-setupInput()
 
 module.exports = setupInput;
