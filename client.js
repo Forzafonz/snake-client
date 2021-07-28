881e72e3f23a482c7e54893a84bb8df94162749e
Anton <golubin1990@gmail.com>
2021-07-28 15:13:22 -0400

    Minor changes

--- client.js
+++ client.js
@@ -8,20 +8,10 @@ const connect = function () {
 // interpret incoming data as text
   conn.setEncoding("utf8");
   conn.on('connect', ()=>{
-    console.log("Connected with this message:");
+    console.log("Connected!")
     conn.write("Name: BAM");
   });
-  // conn.on('connect', ()=>{
-  //   let moves = ["Move: up", "Move: up", "Move: left", "Move: left", "Move: down" , "Move: down" , "Move: right", "Move: right"]
-  //   conn.write("Move: up");
-  //   for (let i = 0; i < 10000; i++){
-  //     let move = moves[Number(i%8)];
-  //     setTimeout(() => conn.write(move), 100 * i);
-  //   }
-  // });
   return conn;
 };
-
-console.log("Connecting ...");
 
 module.exports = connect;
